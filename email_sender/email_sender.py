80ef5da5298367390f09ed32688c53772efe5ec3
vipexi <ville.vihtalahti@student.samk.fi>
2021-01-04 13:45:02 +0200

    Making the new folder for spring.

--- email_sender/email_sender.py
+++ email_sender/email_sender.py
@@ -1,2 +1,14 @@
 #! usr/bin/python3
 
+import 
+
+if __name__ == "__main__":
+
+    filename = ""
+    try:
+        file_to_write = open(filename,"a")
+
+    except:
+        print("cannot open file")
+    
+    file_to_write.write("")
