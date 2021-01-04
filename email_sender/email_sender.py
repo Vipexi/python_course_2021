c45ac80a794fe2c6118c7b75b8039404c3ecc4aa
vipexi <ville.vihtalahti@student.samk.fi>
2021-01-04 14:55:57 +0200

    Sending email, while, function and testing

--- email_sender/email_sender.py
+++ email_sender/email_sender.py
@@ -1,14 +1,60 @@
-#! usr/bin/python3
+#! /usr/bin/python3
 
-import 
+import smtplib
+from email.message import EmailMessage
+
+
+def send_email(subject,body):
+    gmail_user = "vipezi13@gmail.com"
+
+    with open("password.txt") as filereader:
+        gmail_password = filereader.read()
+
+
+    to = ['vipezi13@gmail.com']
+    #subject ='error from application'
+    #body = 'error message'
+
+    msg = EmailMessage ()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = gmail_user
+    msg['To'] = to
+
+    while True:
+        try:
+            server = smtplib.SMTP('smtp.gmail.com',587)
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
+            
+        except:
+            print('cannot connect to server')
+            break
+
+        try:
+            server.login(gmail_user, gmail_password)
+            
+        except:
+            print('error with user name or password')
+            break
+        
+        try:
+            server.send_message(msg)
+            
+        except:
+            print('error with sending a mail')
+            break
+        
+        try:
+            server.close()
+
+        except:
+            print('error with closing server connection')
+            break
+        break
+
+
 
 if __name__ == "__main__":
-
-    filename = ""
-    try:
-        file_to_write = open(filename,"a")
-
-    except:
-        print("cannot open file")
-    
-    file_to_write.write("")
+    send_email("Testing with main","Testing passed!")
